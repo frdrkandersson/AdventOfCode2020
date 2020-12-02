6f53b25ea9f64aa3c4b17c0bd6c8e598e2701409
Fredrik Andersson <andersson.fredrik2@gmail.com>
2020-12-02 22:11:41 +0100

    cleaned up unused code day2

--- Day2/solution.py
+++ Day2/solution.py
@@ -14,31 +14,7 @@ def parseData(data):
     output.append((int(range[0]), int(range[1]), char, input[2]))
   return output
 
-def isPasswordValid2(pw):  
-  input = pw.split()   
-  range = input[0].split('-')
-  pos1 = int(range[0])
-  pos2 = int(range[1])
-  char = input[1].replace(':', '')
-  pw = input[2]    
-
-  bool1 = char == pw[pos1-1]
-  bool2 = char == pw[pos2-1]
-
-  if bool1 != bool2:
-    return 1
-
-  # if count in range(min, max):
-  #   return 1
-  return 0
-
-def part1(data):  
-  ans = 0
-  for i in data:    
-    ans += isPasswordValid2(i)    
-  return ans  
-
-def p1(data):
+def part1(data):
   ans = 0
   for i in data:
     min, max, char, pw = i
@@ -47,7 +23,7 @@ def p1(data):
       ans += 1
   return ans
 
-def p2(data):
+def part2(data):
   ans = 0
   for i in data:
     pos1, pos2, char, pw = i
@@ -56,5 +32,5 @@ def p2(data):
   return ans
 
 input = parseData(data)
-print(p1(input))
-print(p2(input))
+print(part1(input))
+print(part2(input))
