4177fb4069a1bdb5afd4a601d8a2bea0218d7e6c
Fredrik Andersson <andersson.fredrik2@gmail.com>
2020-12-03 22:26:09 +0100

    clean up

--- Day3/solution.py
+++ Day3/solution.py
@@ -3,12 +3,11 @@ from os.path import abspath, dirname, join
 filename = abspath(join(dirname(__file__), 'input.txt'))
 data = open(filename, 'r').read().splitlines()
 max_x = len(data[0])
-max_y = len(data)
 
-def part1(dx, dy):
+def getHits(dx, dy):
   counter = 0
   x = 0
-  for y in range(dy, max_y, dy):
+  for y in range(dy, len(data), dy):
     x += dx
     if x >= max_x:      
       x -= max_x    
@@ -18,8 +17,5 @@ def part1(dx, dy):
 
   return counter
 
-def part2():
-  return part1(1, 1) * part1(3, 1) * part1(5, 1) * part1(7, 1) * part1(1, 2)  
-
-print(part1(3, 1))
-print(part2())
+print(getHits(3, 1))
+print(getHits(1, 1) * getHits(3, 1) * getHits(5, 1) * getHits(7, 1) * getHits(1, 2))
