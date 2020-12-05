e425c1b7c2e3213b7a024cf5daf6e865aec73a3d
Fredrik Andersson <andersson.fredrik2@gmail.com>
2020-12-05 23:46:58 +0100

    clean up day 5

--- Day5/solution.py
+++ Day5/solution.py
@@ -1,5 +1,4 @@
 from os.path import abspath, dirname, join
-import re
 import math
 
 with open(abspath(join(dirname(__file__), 'input.txt')), 'r') as f:
@@ -8,10 +7,7 @@ with open(abspath(join(dirname(__file__), 'input.txt')), 'r') as f:
 def getId(str, min, max, upper, lower):
   for c in str:
     if max-min == 1:
-      if c == lower:
-        return min
-      else:
-        return max
+      return min if c == lower else max      
     if c == upper:
       min += math.ceil((max-min) / 2)
     else:      
@@ -21,10 +17,8 @@ def getSeatIds(data):
   highest = 0
   seatIds = []
   for i in data:
-    rowStr, colStr = i[:7], i[7:]   
-    
-    row = getId(rowStr, 0, 127, 'B', 'F')
-    col = getId(colStr, 0, 7, 'R', 'L')        
+    rowStr, colStr = i[:7], i[7:]      
+    row, col = getId(rowStr, 0, 127, 'B', 'F'), getId(colStr, 0, 7, 'R', 'L')     
     seatId = row * 8 + col
     seatIds.append(seatId)
     highest = max(highest, seatId)
@@ -36,7 +30,7 @@ def part1(data):
     
 def part2(data):  
   ids, _ = getSeatIds(data)
-  list.sort(ids)    
+  list.sort(ids) 
   last = -1
   for id in ids:
     if id - last == 2 and last != -1: 
